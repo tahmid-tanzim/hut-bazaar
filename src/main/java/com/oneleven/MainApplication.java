2a273937d38dafa508c13fd838fea802d99deb07
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2021-06-04 17:38:59 +0600

    Dependency Injection Hands-on

--- src/main/java/com/oneleven/MainApplication.java
+++ src/main/java/com/oneleven/MainApplication.java
@@ -1,13 +1,20 @@
 package com.oneleven;
 
+import com.oneleven.department.Department;
+import com.oneleven.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class MainApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MainApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(MainApplication.class, args);
+
+        Department t = context.getBean(Department.class);
+        t.display();
+        System.out.println(t.toString());
     }
 
 }
